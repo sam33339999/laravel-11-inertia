3ea6d89c3077cb1f8a0fa9387bcab3f9cbb2a4af
sam.zheng <sam33339999@gmail.com>
2025-01-20 23:06:09 +0800

    ts, vite config

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,25 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue';
-import Components from 'unplugin-vue-components/vite';
-import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
+import { defineConfig } from 'vite'
+import laravel from 'laravel-vite-plugin'
+import vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite'
+import path from 'path'
+
+console.log(path.resolve(__dirname, './resources'))
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-        vue(),
-        Components({
-            resolvers: [
-              AntDesignVueResolver({
-                importStyle: false, // css in js
-              }),
-            ],
-        }),
-    ],
-    resolve: name => {
-        const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
-        return pages[`./Pages/${name}.vue`]
+  plugins: [
+    laravel({
+      input: ['resources/css/app.css', 'resources/js/app.js'],
+      refresh: true,
+    }),
+    vue(),
+    Components({
+      resolvers: [],
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './resources'),
     },
-});
+  },
+})
